d8f759a12412f316744a86704997f9392ccd6cfc
Dávid Halász <skateman@skateman.eu>
2021-09-22 09:42:52 +0200

    Implemented tests

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ Floorist is a simple script to dump SQL queries from a PostgreSQL database into 
 The tool has been designed for use in Kubernetes or OpenShift, but you can also run it locally by using `docker` or `podman`. It can be configured through environment variables and a single floorplan (YAML) file.
 
 ```bash
-podman build -t floorist .
+docker build -t floorist .
 
-podman run --rm floorist \
+docker run --rm floorist \
   -e POSTGRES_SERVICE_HOST=localhost \
   -e POSTGRESQL_USER=root \
   -e POSTGRESQL_PASSWORD=123456 \
@@ -47,12 +47,26 @@ The floorplan file simply defines a list of a prefix-query pair. The prefix shou
 
 The example above will create two dumps under the S3 bucket specified in the `AWS_BUCKET` environment variable into the `<prefix>/year_created=<Y>/month_created=<M>/day_created=<D>/<UUID>.parquet` file.
 
+## Testing
+For testing the tool, you will need PostgreSQL and minio, there's a Docker Compose file provided in the `test` folder with everything prepared and configured. The configuration for these two services has to be stored in the `test/env.yaml` file, for the Docker Compose setup it's enough to copy the the `test/env.yaml.example` to make it work. However, if you would like to bring your own PostgreSQL server or maybe use a real S3 bucket, you have to edit these values accordingly. The tests can be started via `pytest`.
+
+```bash
+pip install -r app-src/requirements.txt
+pip install pytest
+cp test/env.yaml.example test/env.yaml
+docker-compose -f test/docker-compose.yml up -d
+pytest
+docker-compose -f test/docker-compose.yml down
+````
+
 ## Contributing
 Bug reports and pull requests are welcome, here are some ideas for improvement:
 * More fine-grained specification of the output path and filename in the floorplan (e.g. custom timestamps)
 * Additional parameters for the parquet exporting (column partitioning, compression, indexing, column types)
 * Support for other databases
 * Support for different object storage services
+* Better tests
+* Pylint support
 
 ## License
 The application is available as open source under the terms of the [Apache License, version 2.0](https://opensource.org/licenses/Apache-2.0).
--- app-src/app.py
+++ app-src/app.py
@@ -1,31 +1,65 @@
+#!/usr/bin/env python3
+
 from datetime import date
 from os import environ as env
 from uuid import uuid4 as uuid
+from s3fs import S3FileSystem as s3
 
+import logging
 import pandas.io.sql as sqlio
 import psycopg2
 import yaml
 
-conn = psycopg2.connect(
-  host=env['POSTGRES_SERVICE_HOST'],
-  user=env['POSTGRESQL_USER'],
-  password=env['POSTGRESQL_PASSWORD'],
-  database=env['POSTGRESQL_DATABASE']
-)
+def main():
+  # Fails if can't connect to S3 or the bucket does not exist
+  s3(client_kwargs={'endpoint_url': env.get('AWS_ENDPOINT') }).ls(env['AWS_BUCKET'])
+  logging.debug('Successfully connected to the S3 bucket')
 
-with open(env['FLOORPLAN_FILE'], 'r') as stream:
-  for row in yaml.safe_load(stream):
-    data = sqlio.read_sql_query(row['query'], conn)
-    target = "/".join([
-      f's3://{env["AWS_BUCKET"]}',
-      row['prefix'],
-      date.today().strftime("year_created=%Y/month_created=%-m/day_created=%-d"),
-      f'{uuid()}.parquet'
-    ])
+  conn = psycopg2.connect(
+    host=env['POSTGRES_SERVICE_HOST'],
+    user=env['POSTGRESQL_USER'],
+    password=env['POSTGRESQL_PASSWORD'],
+    database=env['POSTGRESQL_DATABASE']
+  )
+  logging.debug('Successfully connected to the database')
 
-    data.to_parquet(
-      path=target,
-      compression='gzip',
-      index=False,
-      storage_options={'client_kwargs':{'endpoint_url': env.get('AWS_ENDPOINT') }}
-    )
+  error = False
+  dump_count = 0
+  dumped_count = 0
+
+  with open(env['FLOORPLAN_FILE'], 'r') as stream:
+    # This try block allows us to proceed if a single SQL query fails
+    for row in yaml.safe_load(stream):
+      dump_count += 1
+
+      try:
+        logging.debug(f"Dumping #{dump_count}: {row['query']} to {row['prefix']}")
+
+        data = sqlio.read_sql_query(row['query'], conn)
+        target = '/'.join([
+          f"s3://{env['AWS_BUCKET']}",
+          row['prefix'],
+          date.today().strftime('year_created=%Y/month_created=%-m/day_created=%-d'),
+          f"{uuid()}.parquet"
+        ])
+
+        data.to_parquet(
+          path=target,
+          compression='gzip',
+          index=False,
+          storage_options={'client_kwargs':{'endpoint_url': env.get('AWS_ENDPOINT') }}
+        )
+
+        logging.debug(f"Dumped #{dumped_count}: {row['query']} to {row['prefix']}")
+
+        dumped_count += 1
+      except Exception as ex:
+        logging.exception(ex)
+
+  logging.info(f'Dumped {dumped_count} from total of {dump_count}')
+
+  if dumped_count != dump_count:
+    exit(1)
+
+if __name__ == '__main__':
+  main()
