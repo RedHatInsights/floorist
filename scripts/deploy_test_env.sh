326e0c31d837da6799f5b8a9e02a2a6f6c1a365e
patchkez <patchkez@protonmail.com>
2026-05-06 13:51:07 +0200

    updated deploy_test_env.sh

--- scripts/deploy_test_env.sh
+++ scripts/deploy_test_env.sh
@@ -1,4 +1,66 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
 echo "Started deploy_test_env.sh script"
-pwd
+
+
+
+# echo "Image URL: ${IMAGE_URL}"
+# echo "Image Digest: ${IMAGE_DIGEST}"
+#
+# export POD_NAME="TEST_POD"
+# export JOB_NAME="TEST_JOB"
+# export CONFIGMAP_NAME="postgresql_cm"
+# export TMPDIR="/tmp"
+#
+#
+#
+# echo "Creating pod YAML..."
+# oc create configmap ${CONFIGMAP_NAME} --from-file=enable-extensions.sh
+#
+#
+# echo "Creating pod YAML..."
+# oc process -f templates/test-pod-template.yaml -o yaml \
+#     -p IMAGE_URL="${IMAGE_URL}" \
+#     -p POD_NAME="${POD_NAME}" \
+#     > "${TMPDIR}/${POD_NAME}.yaml"
+#
+# echo ""
+# echo "=== Applying Pod YAML ==="
+# cat "${TMPDIR}/${POD_NAME}.yaml"
+# oc apply -f "${TMPDIR}/${POD_NAME}"
+# sleep 10
+# oc get pods
+#
+#
+# oc apply -f tests/templates/service.yaml
+# oc get services
+#
+#
+# echo ""
+# echo "=== Creating job YAML... ==="
+# oc process -f templates/test-job-template.yaml -o yaml \
+#     -p IMAGE_URL="${IMAGE_URL}" \
+#     -p JOB_NAME="${JOB_NAME}" \
+#     > "${TMPDIR}/${JOB_NAME}.yaml"
+#
+# echo ""
+# echo "=== Applying Job YAML ==="
+# cat "${TMPDIR}/${JOB_NAME}.yaml"
+#
+# oc apply -f "${TMPDIR}/${JOB_NAME}.yaml"
+#
+# echo ""
+# echo "Waiting for job to complete..."
+# oc wait --for=condition=complete --timeout=60s "job/${JOB_NAME}"
+#
+# echo ""
+# echo "=== Job Status ==="
+# oc get jobs
+#
+# echo ""
+# echo "=== Job Logs ==="
+# oc logs "job/${JOB_NAME}"
+#
+# oc delete "pod/${POD_NAME}"
+# oc delete "job/${JOB_NAME}"
